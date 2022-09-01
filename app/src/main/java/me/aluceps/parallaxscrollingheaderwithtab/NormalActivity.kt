3371286212a1e75393d5247d061f8c5fe94cc8a3
atsushi.takano <atsushi.takano@every.tv>
2022-09-01 15:06:00 +0900

    画面を追加

---
+++ app/src/main/java/me/aluceps/parallaxscrollingheaderwithtab/NormalActivity.kt
@@ -0,0 +1,26 @@
+package me.aluceps.parallaxscrollingheaderwithtab
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.aluceps.parallaxscrollingheaderwithtab.databinding.ActivityNormalBinding
+
+class NormalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityNormalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            title = "あいうえお株式会社"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    companion object {
+        fun start(context: Context) = context.startActivity(
+            Intent(context, NormalActivity::class.java)
+        )
+    }
+}
