63ade2947e862060b97effa10408d4688a9d2ee3
atsushi.takano <atsushi.takano@every.tv>
2022-09-01 17:46:09 +0900

    タブの中身を追加

--- app/src/main/java/me/aluceps/parallaxscrollingheaderwithtab/TabActivity.kt
+++ app/src/main/java/me/aluceps/parallaxscrollingheaderwithtab/TabActivity.kt
@@ -2,10 +2,13 @@ package me.aluceps.parallaxscrollingheaderwithtab
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.CollapsingToolbarLayout.TITLE_COLLAPSE_MODE_FADE
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import me.aluceps.parallaxscrollingheaderwithtab.databinding.ActivityTabBinding
 
 class TabActivity : AppCompatActivity() {
@@ -18,12 +21,39 @@ class TabActivity : AppCompatActivity() {
             title = "あいうえお株式会社"
             setDisplayHomeAsUpEnabled(true)
         }
+        setupView(binding)
     }
 
     override fun onSupportNavigateUp(): Boolean =
         onBackPressed().let {
             true
         }
+
+    private fun setupView(binding: ActivityTabBinding) {
+        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = (binding.viewPager.adapter as? ViewPagerAdapter)?.getTitle(position) ?: ""
+        }.attach()
+    }
+
+    private class ViewPagerAdapter(
+        manager: FragmentManager,
+        lifecycle: Lifecycle,
+    ) : FragmentStateAdapter(manager, lifecycle) {
+        enum class Tabs(val title: String) {
+            PAGE1("レシピ"),
+            PAGE2("記事"),
+        }
+
+        fun getTitle(position: Int) =
+            Tabs.values()[position].title
+
+        override fun getItemCount(): Int =
+            Tabs.values().size
+
+        override fun createFragment(position: Int): Fragment =
+            TabFragment.newInstance()
+    }
 
     companion object {
         fun start(context: Context) = context.startActivity(
---
+++ app/src/main/java/me/aluceps/parallaxscrollingheaderwithtab/TabFragment.kt
@@ -0,0 +1,22 @@
+package me.aluceps.parallaxscrollingheaderwithtab
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.aluceps.parallaxscrollingheaderwithtab.databinding.FragmentTabBinding
+
+class TabFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentTabBinding.inflate(inflater, container, false).root
+    }
+
+    companion object {
+        fun newInstance() = TabFragment()
+    }
+}
