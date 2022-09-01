cff627e77c9059bcb174fc1cb354c41358ac618e
atsushi.takano <atsushi.takano@every.tv>
2022-09-01 12:48:25 +0900

    空の画面を追加

---
+++ app/src/main/java/me/aluceps/parallaxscrollingheaderwithtab/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.aluceps.parallaxscrollingheaderwithtab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.aluceps.parallaxscrollingheaderwithtab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
